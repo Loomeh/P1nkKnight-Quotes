f03821b30dc494597be6c23109ccc345ad0a76d2
Loomeh <67561520+Loomeh@users.noreply.github.com>
2020-06-30 18:58:10 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//Brobey Quotes Bot by Loomeh. You expectin' some copyright shit? nah lol.
+
+$m1 ="mmm, butter me up.";
+$m2 ="You wan' me to cook you a juicy schnacc?";
+$m3 ="The feetpics gon' cost ya!";
+$m4 ="Ya like these buns?";
+$rm=array($m1, $m2, $m3, $m4);
+$random_keys = array_rand($rm, 1);
+echo $rm[$random_keys];
+?>
