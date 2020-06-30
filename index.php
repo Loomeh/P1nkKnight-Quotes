09e15bf4b35a97664f1f6a3940b25deb0297a14d
Loomeh <67561520+Loomeh@users.noreply.github.com>
2020-06-30 21:17:20 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ $m1 ="mmm, butter me up.";
 $m2 ="You wan' me to cook you a juicy schnacc?";
 $m3 ="The feetpics gon' cost ya!";
 $m4 ="Ya like these buns?";
-$rm=array($m1, $m2, $m3, $m4);
+$m5 ="doRitOOOOO! I like cool ranch."
+$rm=array($m1, $m2, $m3, $m4, $m5);
 $random_keys = array_rand($rm, 1);
 echo $rm[$random_keys];
 ?>
